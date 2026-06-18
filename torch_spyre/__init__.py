b567a6449ef892a77d419241480ddc22a307ad17
Antoni Viros <aviros@ibm.com>
2026-06-18 09:44:29 -0400

    Apply the Spyre tensor monkey-patch at autoload time (decouple from runtime init) (#2687)
    * Fix autoload/lazy init to allow STL moves
    
    Signed-off-by: Antoni Viros i Martin <aviros@ibm.com>
    
    * Avoid _C import and add new test
    
    Signed-off-by: Antoni Viros i Martin <aviros@ibm.com>
    
    * Split tests
    
    Signed-off-by: Antoni Viros i Martin <aviros@ibm.com>
    
    * GHA config
    
    Signed-off-by: Antoni Viros i Martin <aviros@ibm.com>
    
    * GHA pt2
    
    Signed-off-by: Antoni Viros i Martin <aviros@ibm.com>
    
    ---------
    
    Signed-off-by: Antoni Viros i Martin <aviros@ibm.com>

--- torch_spyre/__init__.py
+++ torch_spyre/__init__.py
@@ -256,7 +256,6 @@ def _autoload():
     _autoload._ran = True
 
     import torch  # noqa: E402
-    from . import _C  # noqa: F401
 
     # Set all the appropriate state on PyTorch
     torch.utils.rename_privateuse1_backend(DEVICE_NAME)
